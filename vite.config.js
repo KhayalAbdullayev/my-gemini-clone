4adb6df7146eb71ca46939fdead7b984c154347e
Khayal Abdullayev <xeyalx859@gmail.com>
2025-07-25 15:46:30 +0300

    fix: add rollup config for marked

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/my-gemini-clone"
+  base: "/my-gemini-clone",
+  build: {
+    rollupOptions: {
+      external: ['marked'], // Add this line
+      output: {
+        manualChunks: {
+          marked: ['marked'] // Optional optimization
+        }
+      }
+    }
+  }
 })
